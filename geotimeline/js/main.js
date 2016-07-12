fdce9090ffcd3a4111c0c2f9b382201d7e2836c3
Andrew Kramer <ajkramer12@gmail.com>
2016-07-11 20:38:42 -0400

    Began rudimentatry d3/GoogleMap navigation linking

--- geotimeline/js/main.js
+++ geotimeline/js/main.js
@@ -1,10 +1,13 @@
+var map
+
 function initialize() {
     var mapProp = {
-        center:new google.maps.LatLng(51.508742,-0.120850),
-        zoom:5,
-        mapTypeId:google.maps.MapTypeId.ROADMAP
+        center:new google.maps.LatLng(30,0),
+        zoom:2,
+        mapTypeId:google.maps.MapTypeId.SATELLITE,
+        disableDefaultUI: true
     };
-    var map=new google.maps.Map(document.getElementById("primaryMapColumn"),mapProp);
+    map=new google.maps.Map(document.getElementById("primaryMapColumn"),mapProp);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
@@ -13,27 +16,45 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
-var width = d3.select("#primaryMap").style("width").slice(0, -2);
-var height = d3.select("#primaryMap").style("height").slice(0, -2);
+var width = d3.select("#primaryMapColumn").style("width").slice(0, -2);
+var height = d3.select("#primaryMapColumn").style("height").slice(0, -2);
+//var latitude = height / 1.53;
+var latitude = height / 2;
+var longitude = width / 2;
 
-var primaryMap = d3.select("#primaryMap");
+var primaryMap = d3.select("#primaryMap")
+                .attr("width", width)
+                .attr("height", height)
+                .append("g");
 
 // Set projection path
 var projection = d3.geo.mercator()
-    .translate([width / 2, height / 2])
-    .scale(100);
+    //.translate([width / 2, height / 1.43])
+    .center([0, 30])
+    .translate([longitude, latitude])
+    .scale(163);
+    //.scale((width - 1) / 2 / Math.PI);
+
+/*var zoom = d3.behavior.zoom()
+    .scaleExtent([1, 3])
+    .on("zoom", zoomed);
+*/
 
 var path = d3.geo.path()
     .projection(projection);
 
+/*primaryMap
+    .call(zoom);
+    //.call(zoom.event);
+*/
 queue()
     .defer(d3.json, "data/world-110m.json")
-    .await(temp);
+    .await(renderData);
 
-//temp
-function temp() {
-    window.setTimeout(renderData, 3000);
-}
+
+var miniMap = d3.select("#miniMap")
+    .on("mouseover", travel);
+
 
 
 // Render Data
@@ -53,10 +74,37 @@ function renderData(error, mapData) {
 
 }
 
+function zoomed() {
+    var e = d3.event;
+    //var tx = Math.min(0, Math.max(e.translate[0], width - width * e.scale));
+    //var ty = Math.min(0, Math.max(e.translate[1], height - height * e.scale));
+    var x = e.sourceEvent.x;
+    var y = e.sourceEvent.y;
+    var tx = e.translate[0];
+    var ty = e.translate[1];
+    zoom.translate([tx, ty]);
 
 
+    //primaryMap.attr("transform", "translate(" + [tx, ty] + ")scale(" + e.scale + ")");
+    //projection.center([tx, ty]);
+    //console.log(tx+", "+ty+" | "+ e.scale);
+    //console.log(projection.center());
+    console.log(projection.invert([x, y]));
+    //console.log(x + " " + y);
+    longitude = tx;
+    latitude = ty;
+}
 
+function travel(){
+    var pCenter = projection.center();
+    projection.center([pCenter[0]-10,pCenter[1]]);
+    console.log(projection.center());
 
+    primaryMap.selectAll("path")
+        .transition()
+        .duration(0)
+        .attr("d", path);
 
-
-
+    var translate = projection.translate();
+    map.setCenter({lat: pCenter[1], lng: pCenter[0]-10});
+}
