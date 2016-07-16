d342774c306d231fcc37bdbe1bc1a3f8d07818f0
Andrew Kramer <ajkramer12@gmail.com>
2016-07-15 23:45:34 -0400

    Completed Minimap

--- geotimeline/js/main.js
+++ geotimeline/js/main.js
@@ -12,7 +12,7 @@ var longitude = width / 2;
 
 var currentZoom = 0;
 var zoomConversionD3 = 163;
-var zoomConversionGoogle = 2
+var zoomConversionGoogle = 2;
 
 
 // Declare/Initialize map variables
@@ -84,6 +84,8 @@ function initializeGoogleMap() {
 /* User Triggers & Functions */
 var zoomLock = false;
 var zoomInterval = 200;
+var baseTravelSpeed = 5;
+var travelSpeed = baseTravelSpeed;
 
 d3.select("body").on("wheel.zoom", function(){
     if(!zoomLock) {
@@ -129,10 +131,22 @@ function zoom(direction) {
         googleMap.setZoom(currentZoom + zoomConversionGoogle);
     }
 
+    // Redraw map
     primaryMap.selectAll("path")
         .transition()
         .duration(zoomInterval)
         .attr("d", path);
+
+    //Redraw minimap viewbox
+    miniMapSizeConversion = miniProjection.scale()/projection.scale();
+    viewBox
+        .transition()
+        .duration(500)
+        .attr("x", miniProjection(projection.center())[0] - width * miniMapSizeConversion/2)
+        .attr("y", miniProjection(projection.center())[1] - height * miniMapSizeConversion/2)
+        .attr("width", width * miniMapSizeConversion)
+        .attr("height", height * miniMapSizeConversion);
+
 
     if(currentZoom == 0){
         projection.center([0,30]);
@@ -142,11 +156,26 @@ function zoom(direction) {
             .duration(0)
             .attr("d", path);
 
+        // Disable primary map navigation
         d3.selectAll(".panTrigger")
             .classed("hidden", true);
+
+        // Disable minimap viewbox
+        viewBox
+            .attr("fill", "none")
+            .attr("stroke", "none");
     } else {
+        // Update Travel speed
+        travelSpeed = Math.floor(baseTravelSpeed / currentZoom);
+
+        // Enable primary map navigation
         d3.selectAll(".panTrigger")
             .classed("hidden", false);
+
+        // Enable minimap viewbox navigation
+        viewBox
+            .attr("fill", "rgba(255,240,240,.1)")
+            .attr("stroke", "rgba(255,0,0,1)");
     }
 }
 
@@ -160,23 +189,23 @@ function travel(direction){
     // Calculate new map center coordinates & update D3 projection & Google Map position
     switch(direction){
         case 0: if(pCenter[1] < 70) {
-                    projection.center([pCenter[0], pCenter[1] + 10]);
-                    googleMap.setCenter({lat: pCenter[1] + 10, lng: pCenter[0]});
+                    projection.center([pCenter[0], pCenter[1] + travelSpeed]);
+                    googleMap.setCenter({lat: pCenter[1] + travelSpeed, lng: pCenter[0]});
                 }
                 break;
         case 1: if(pCenter[0] < 180) {
-                    projection.center([pCenter[0]+10,pCenter[1]]);
-                    googleMap.setCenter({lat: pCenter[1], lng: pCenter[0]+10});
+                    projection.center([pCenter[0]+travelSpeed,pCenter[1]]);
+                    googleMap.setCenter({lat: pCenter[1], lng: pCenter[0]+travelSpeed});
                 }
                 break;
         case 2: if(pCenter[1] > -70) {
-                    projection.center([pCenter[0], pCenter[1] - 10]);
-                    googleMap.setCenter({lat: pCenter[1] - 10, lng: pCenter[0]});
+                    projection.center([pCenter[0], pCenter[1] - travelSpeed]);
+                    googleMap.setCenter({lat: pCenter[1] - travelSpeed, lng: pCenter[0]});
                 }
                 break;
         case 3: if(pCenter[0] > -180) {
-                    projection.center([pCenter[0] - 10, pCenter[1]]);
-                    googleMap.setCenter({lat: pCenter[1], lng: pCenter[0] - 10});
+                    projection.center([pCenter[0] - travelSpeed, pCenter[1]]);
+                    googleMap.setCenter({lat: pCenter[1], lng: pCenter[0] - travelSpeed});
                 }
                 break;
     }
@@ -186,6 +215,14 @@ function travel(direction){
         .transition()
         .duration(0)
         .attr("d", path);
+
+    //Redraw minimap viewbox
+    miniMapSizeConversion = miniProjection.scale()/projection.scale();
+    viewBox
+        .attr("x", miniProjection(projection.center())[0] - width * miniMapSizeConversion/2)
+        .attr("y", miniProjection(projection.center())[1] - height * miniMapSizeConversion/2)
+        .attr("width", width * miniMapSizeConversion)
+        .attr("height", height * miniMapSizeConversion);
 }
 
 
@@ -205,6 +242,8 @@ var miniWidth = d3.select("#miniMapColumn").style("width").slice(0, -2);
 var miniHeight = d3.select("#miniMapColumn").style("height").slice(0, -2);
 var miniMapCenterY = miniHeight / 2;
 var miniMapCenterX = miniWidth / 2;
+var miniMapSizeConversion;
+var viewBox;
 
 
 // Initialize map variable
@@ -224,10 +263,17 @@ var miniProjection = d3.geo.mercator()
 var miniMapBase = d3.geo.path()
     .projection(miniProjection);
 
+// Create the drag behavior for the view box
+var viewBoxDrag = d3.behavior.drag()
+    .on("drag", viewBoxDragBehavior)
+    .on("dragend", viewBoxDragRelease);
+
 // Load map data
 queue()
     .defer(d3.json, "data/world-110m.json")
     .await(initializeMiniMap);
+
+
 
 function initializeMiniMap(error, mapData) {
 
@@ -241,4 +287,38 @@ function initializeMiniMap(error, mapData) {
         .attr("d", miniMapBase)
         .attr("class", "country");
 
+    //Render View Box
+    miniMapSizeConversion = miniProjection.scale()/projection.scale();
+
+    viewBox = miniMap.append("rect")
+        .attr("id", "viewBox")
+        .attr("x", miniProjection(projection.center())[0] - width * miniMapSizeConversion/2)
+        .attr("y", miniProjection(projection.center())[1] - height * miniMapSizeConversion/2)
+        .attr("width", width * miniMapSizeConversion)
+        .attr("height", height * miniMapSizeConversion)
+        .attr("fill", "none")
+        .attr("stroke-width", 2)
+        .attr("stroke", "none")
+        .call(viewBoxDrag);
+
+}
+
+function viewBoxDragBehavior() {
+
+    d3.select(this)
+        .attr("x", d3.event.x - viewBox.attr("width")/2)
+        .attr("y", d3.event.y - viewBox.attr("height")/2);
+}
+function viewBoxDragRelease() {
+    var vCenter = miniProjection.invert([d3.event.sourceEvent.offsetX, d3.event.sourceEvent.offsetY]);
+    console.log(vCenter);
+    console.log(d3.event.sourceEvent);
+    // Update Primary Map
+    projection.center(vCenter);
+    googleMap.setCenter({lat: vCenter[1], lng: vCenter[0]});
+    // Update D3 map position
+    primaryMap.selectAll("path")
+        .transition()
+        .duration(0)
+        .attr("d", path);
 }
